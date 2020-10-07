633b1d81507e87233048684731801eeb7e520bb6
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-10-07 15:19:47 -0500

    add formss - finish

---
+++ src/pages/Criterios/inedex.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Criterios: React.FC = () => {
+    return (
+        <>
+            <div className="App">
+
+            </div>
+        </>
+    )
+}
+
+export default Criterios;
